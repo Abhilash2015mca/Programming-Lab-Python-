c71f9cc1cd6078155e21a6c39163d3b949766e70
Abhilash2015mca <abhilashjohn64@gmail.com>
2021-02-01 11:55:59 +0530

    1stfeb

---
+++ example/DictionarySquareModule/dict.py
@@ -0,0 +1,5 @@
+def sq(num1,num2):
+    dic = {}
+    for i in range(num1,num2):
+        dic[i] = num1**2
+    print(dic)
---
+++ example/DictionarySquareModule/numbers.py
@@ -0,0 +1,4 @@
+import dict
+n = int(input("Enter the first number"))
+m = int(input("Enter the last number"))
+my_dict = dict.sq(n,m)
