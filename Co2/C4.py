92bedaee09c268ef71fb1dc2887dbb6dfa8d1777
Abhilash2015mca <abhilashjohn64@gmail.com>
2021-03-14 10:17:15 +0530

    10.17

--- Co2/C4.py
+++ Co2/C4.py
@@ -4,7 +4,6 @@ import math
 b=int(input("Enter range from:"))
 c=int(input("Enter range to:"))
 a=[]
-print(b**2)
 if b>999 & c<10000:
     for x in range(b,c+1):
         if x%2==0 and math.isqrt(x) ** 2 == x:
