f82ced521ff6bb08beffc02ddaf02246ce14d469
Abhilash2015mca <abhilashjohn64@gmail.com>
2021-03-14 09:50:49 +0530

    9.50

--- Co4/3.py
+++ Co4/3.py
@@ -1,20 +1,19 @@
 #Create a class Rectangle with private attributes length and width. Overload ‘<’ operator to 
 #compare the area of 2 rectangles.
-class rectangle:
-    def __init__(self,length,width):
-        self.length=length
-        self.width=width
-    def area():
-        self.area=self.length*self.width    
-        return self.area
-    def __gt__(self, other): 
-        if(self.area>other.area): 
-            return True
-        else: 
-            return False    
-rec1=rectangle(3,5)
-rec2=rectangle(4,7)
-if(rec1>rec2):
-    print("area of first rectangle is more")
-else:
-    print("Area of second rectangle is more")                
+class Rectangle :
+    def __init__(self,length,breadth):
+        self.__length = length
+        self.__breadth = breadth
+        self.__area = length*breadth
+
+    def __lt__(self, obj):
+
+        if (self.__area<obj.__area):
+            return "Rectangle 1 has larger area"
+        else:
+            return "Rectangle 1 does not have larger area"
+
+
+rec1 = Rectangle(7,4)
+rec2 = Rectangle(123,21)
+print(rec1>rec2)              
---
+++ Co4/4.py
@@ -0,0 +1,2 @@
+#reate a class Time with private attributes hour, minute and second. Overload ‘+’ operator to 
+#find sum of 2 
